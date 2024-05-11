bd935d7745b9482c8de1b87cbcb682b38649b173
Eliott Morcillo <eliott.morcillo@gmail.com>
2024-05-11 14:52:15 +0200

    feat: deploy version

--- simulation/web-client/src/app/(main)/page.tsx
+++ simulation/web-client/src/app/(main)/page.tsx
@@ -4,7 +4,6 @@ import { Footer } from '@/components/landing/Footer'
 import { Header } from '@/components/landing/Header'
 import { Hero } from '@/components/landing/Hero'
 import { Pricing } from '@/components/landing/Pricing'
-import { PrimaryFeatures } from '@/components/landing/PrimaryFeatures'
 import { SecondaryFeatures } from '@/components/landing/SecondaryFeatures'
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
     <>
       <Header />
       <Hero />
-      <PrimaryFeatures />
       <SecondaryFeatures />
       <CallToAction />
       <Pricing />
