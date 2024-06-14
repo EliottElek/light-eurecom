92d864cfda6f78bd1f7e93f704342621b8cb1817
Eliott Morcillo <eliott.morcillo@gmail.com>
2024-06-14 14:08:16 +0200

    fix: one folder per receiver

--- multicast-video-xor/sender/sender.py
+++ multicast-video-xor/sender/sender.py
@@ -1,13 +1,16 @@
 import socket
+import time
 from utils import code_chunks, split_into_chunks, split_video
+
 MULTICAST_GROUP = '224.0.0.1'
 MULTICAST_PORT = 5007
 BUFFER_SIZE = 1024
+TTL = 3  # Time to Live for multicast packets
 
-if __name__ == '__main__':
+def send_multicast():
     print("Starting multicast sender...")
     multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, TTL)
 
     try:
         chunk_size = 2048
@@ -16,15 +19,24 @@ if __name__ == '__main__':
         if v1_chunks is not None and v2_chunks is not None:
             coded_chunk = code_chunks(v1_chunks[1], v2_chunks[0])
 
-        coded_chunks_2048 = split_into_chunks(coded_chunk, 2048)
+        coded_chunks_2048 = split_into_chunks(coded_chunk, chunk_size)
         for chunk in coded_chunks_2048:
             multicast_socket.sendto(chunk, (MULTICAST_GROUP, MULTICAST_PORT))
-            if chunk == b'LAST_PACKET':
-                break
             print("Sent chunk of size:", len(chunk))
+
+        # Send the last packet indicator
+        multicast_socket.sendto(b'LAST_PACKET', (MULTICAST_GROUP, MULTICAST_PORT))
+        print("Sent last packet indicator")
+
+        # Adding a short delay to ensure the last packet is sent before closing the socket
+        time.sleep(1)
 
     except OSError as e:
         print("[Error] OS Error:", str(e))
 
     finally:
         multicast_socket.close()
+        print("Multicast socket closed")
+
+if __name__ == '__main__':
+    send_multicast()
